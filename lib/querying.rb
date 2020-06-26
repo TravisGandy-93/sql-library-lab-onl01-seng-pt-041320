876bf2dfc8aa046b9810774190db2cc7efa0711a
Travis Gandy <travis_gandy@yahoo.com>
2020-06-26 14:48:42 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -21,14 +21,11 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name,subgenres.name FROM authors
-  JOIN characters
-  ON authors.id = characters.author_id
-  JOIN series
-  ON characters.series_id = series.id
-  JOIN subgenres
-  ON series.subgenre_id = subgenres.id
-  GROUP BY authors.name;"
+  "SELECT authors.name, subgenres.name FROM authors 
+  JOIN series 
+  ON series.author_id = authors.id 
+  JOIN subgenres 
+  ON series.subgenre_id = subgenres.id"
 end
 
 def select_series_title_with_most_human_characters
